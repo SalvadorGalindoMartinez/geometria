458452098005518e53c5a5c5f8586f3a59d38356
Salvador Galindo Martínez <salva150809@gmail.com>
2019-02-11 21:33:29 +0100

    versión refactorizada

--- geometria/src/Circulo_SGM.java
+++ geometria/src/Circulo_SGM.java
@@ -1,10 +1,10 @@
 
-public class Circulo extends FiguraGeometrica {
+public class Circulo_SGM extends FiguraGeometrica_SGM {
 	private double radio;
 	//Constante pi
 	static final double PI = 3.1416;
 	
-	public Circulo(double r, String tipoFigura) {
+	public Circulo_SGM(double r, String tipoFigura) {
 		super(tipoFigura);
 		radio = r;
 	}
--- geometria/src/FiguraGeometrica_SGM.java
+++ geometria/src/FiguraGeometrica_SGM.java
@@ -1,9 +1,9 @@
 
-public abstract class FiguraGeometrica {
+public abstract class FiguraGeometrica_SGM {
 
 	protected String tipoFigura;
 
-	public FiguraGeometrica(String tipoFigura) {
+	public FiguraGeometrica_SGM(String tipoFigura) {
 		super();
 		this.tipoFigura = tipoFigura;
 	}
